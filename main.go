04be3aaa5eea714d9a1c1f999dcab33f21c2180d
Jonathan Barrow <jonbarrow1998@gmail.com>
2023-04-08 11:02:33 -0400

    Updated NEX lib versions

--- main.go
+++ main.go
@@ -12,9 +12,13 @@ var nexServer *nex.Server
 
 func main() {
 	nexServer = nex.NewServer()
-	nexServer.SetPrudpVersion(1)
+	nexServer.SetPRUDPVersion(1)
 	nexServer.SetPRUDPProtocolMinorVersion(2)
-	nexServer.SetNexVersion(30701)
+	nexServer.SetDefaultNEXVersion(&nex.NEXVersion{
+		Major: 3,
+		Minor: 7,
+		Patch: 1,
+	})
 	nexServer.SetKerberosKeySize(32)
 	nexServer.SetKerberosPassword(os.Getenv("KERBEROS_PASSWORD"))
 	nexServer.SetAccessKey("d8927c3f")
